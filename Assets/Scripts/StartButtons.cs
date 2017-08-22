778e2694c6c294340f870b6ad116bb5f2275ed03
ciderxx <ci___der@naver.com>
2017-08-23 01:57:50 +0900

    Multiple Scene make
    17 08 23

--- Assets/Scripts/StartButtons.cs
+++ Assets/Scripts/StartButtons.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class StartButtons : MonoBehaviour {
     public void StartButton()
